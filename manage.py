63777b3bb1abfde900a5cc5a3c16d17915eef109
Mark Miles <mmiles2@gmail.com>
2020-10-29 18:24:52 -0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'azuresite.production')
     else:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_hello.settings')
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
