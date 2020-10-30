c91ff4ae33465ff9537ed1e329195a712ba167f5
tannerb9 <tanner-brainard@hotmail.com>
2020-10-30 10:30:23 -0500

    feat: created AboutMyFacilitators component

---
+++ biztown/src/components/AboutMyFacilitators.js
@@ -0,0 +1,54 @@
+import React, { useRef, Redirect } from "react"
+import TableRow from "./TableRow"
+import DataManager from "../modules/DataManager";
+import { Table, Button } from 'reactstrap';
+
+const AboutMyFacilitators = () => {
+  const input1 = useRef()
+  const input2 = useRef()
+  const input3 = useRef()
+
+  const inputs = [input1, input2, input3]
+
+  const handleQuestions33To35 = (e) => {
+    e.preventDefault();
+
+    const questions33To35 = {
+      q33_score: input1.current.value,
+      q34_score: input2.current.value,
+      q35_score: input3.current.value
+    }
+
+    DataManager.post("posttest", questions33To35).then(() => {
+      <Redirect to="/" />
+    })
+  }
+
+  const prompts = ["My instructor made a connection between real life and what I learned in the classroom.", "My instructor/volunteer helped me to realize the importance of staying in school.", "My volunteer’s personal stories motivated me."]
+
+  return (
+    <>
+      <Table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>ABOUT MY FACILITATORS...</th>
+            <th>Disagree</th>
+            <th>Slightly Disagree</th>
+            <th>Slightly Agree</th>
+            <th>Agree</th>
+          </tr>
+        </thead>
+        <tbody>
+          {prompts.map((prompt, idx) => (
+            <TableRow prompt={prompt} inputs={inputs} idx={idx} aboutMyFuture={true}/>
+          ))}
+        </tbody>
+      </Table>
+      <Button onClick={handleQuestions33To35}>Submit</Button>
+    </>
+  )
+
+}
+
+export default AboutMyFacilitators
--- biztown/src/components/ApplicationViews.js
+++ biztown/src/components/ApplicationViews.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import AboutMe from "./AboutMe"
 import AboutMyFuture from "./AboutMyFuture"
+import AboutMyFacilitators from "./AboutMyFacilitators"
 
 const ApplicationViews = () => {
 
@@ -17,6 +18,12 @@ const ApplicationViews = () => {
         path="/aboutmyfuture"
         render={()=> (
           <AboutMyFuture />
+        )}
+      />
+      <Route
+        path="/aboutmyfacilitators"
+        render={()=> (
+          <AboutMyFacilitators />
         )}
       />
     </>
